49bbcea857b32f53d7f065f4ef96504b562fe8fb
amandlabeautin <amandlabeautin@gmail.com>
2016-01-09 10:36:51 +0100

    Ajout du popup
    - popup affichant le nom, la rotation et l’orbit de la planète
    - une couleur différente pour chaque planète

--- js/scriptAmandla.js
+++ js/scriptAmandla.js
@@ -2,13 +2,17 @@ d3.json("donnees/planetsSW.json",function(error,data){
         if (error) return console.warn(error);
         var json = data.results;
 
-  var w = 800, h = 800;
+  var w = 800, h = 600;
   var t0 = Date.now();
 
 var svg = d3.select("#graphTerrain").insert("svg")
     .attr("width", w).attr("height", h);
 
-  svg.append("circle").attr("r", 20).attr("cx", w/2)
+var newPopA = d3.select('#graphTerrain').append('div')
+                    .attr('id', "newPop")
+                    .style("display","none");
+
+  svg.append("circle").attr("r", 30).attr("cx", w/2)
     .attr("cy", h/2).attr("class", "sun")
 
   var container = svg.append("g")
@@ -16,17 +20,25 @@ var svg = d3.select("#graphTerrain").insert("svg")
 
   container.selectAll("g.planet").data(json).enter().append("g")
     .attr("class", "planetOrbit").each(function(d, i) {
-      
       d3.select(this).append("circle").attr("class", "orbit")
         .attr("r", d.orbital_period/2);
       d3.select(this).append("circle").attr("r", d.rotation_period/2).attr("cx",d.orbital_period/2)
-        .attr("cy", 0).attr("class", "planetOrbit");
-    });
+        .attr("cy", 0).attr("class", function(d) { return d.name})
+        .on("mousemove", function(d){
+           newPopA.style("display","block")
+                 .text(d.name + " " + "Rotation : " + d.rotation_period + " " + "Orbit : " + d.orbital_period)
+                .style("left", (d3.event.pageX + 16)+ "px")
+                 .style("top", (d3.event.pageY + 16) + "px"); 
+            })
+        .on("mouseout", function(d){
+            newPopA.style("display","none");
+    })
 
   d3.timer(function() {
    var delta = (Date.now() - t0);
     svg.selectAll(".planetOrbit").attr("transform", function(d) {
-      return "rotate(" + 90 + delta * d.diameter/200000 + ")";
+      return "rotate(" + 90 + delta * d.diameter/400000 + ")";
     });
   });
 });
+    });
