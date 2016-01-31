48598ea0047d2d279a2178230b685fd77fed1875
Mehdi <m.batista@outlook.fr>
2016-02-01 00:08:28 +0100

    Changement de l'image de chargement

--- js/chargementprogress.js
+++ js/chargementprogress.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function(){
     $('#container').append('<div class="progress" style="border-radius: 25px;box-shadow: 0px 0px 15px 5px rgba(162, 249, 0, .75);">');
     $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="'+ progress +'" aria-valuemin="0" aria-valuemax="100" style="width: '+ progress +'%;background-color: #A2F900;border-style: solid;">');
     //$('.progress-bar').append('<span id="span class="sr-only">60% Complete</span>');
-     $('#test').append('<div class=".col-md-4" id="imgbarcont" style="text-align: center;clear: left;"><img id="imgbar" src="img/img_vador.png" /></div>');
+     $('#test').append('<div class=".col-md-4" id="imgbarcont" style="text-align: center;clear: left;"><img id="imgbar" src="img/img_vador.gif" /></div>');
     $('#test').append('<div class=".col-md-4" id="imgbarcont" style="text-align: center;clear: left;"><p id="chargencour"> Chargement en cours ...</p></div>');
     ajoutcharg(10);
 });
