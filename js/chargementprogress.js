380e9c202859be84fd10cd1e378abb0a6f211829
Mehdi Batista <m.batista@outlook.fr>
2016-02-02 12:08:40 +0100

    Correction centrage de la barre de chargement CSS

--- js/chargementprogress.js
+++ js/chargementprogress.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
     $('body').append('<div id="test"></div>'); //Add the fade layer to bottom of the body tag.
     $('#test').css({'filter' : 'alpha(opacity=95)'}).fadeIn(); //Fade in the fade layer 
     $('#test').append('<div class=".col-md-4" style="text-align: center;clear: left;"><img id="imgintro" src="img/header_charg.jpg" /></div>');
-    $('#test').append('<div id="container" class = "col-md-6 col-md-offset-3" style="margin-top:3%;left: 4%;"></div>');  
+    $('#test').append('<div id="container" class = "col-md-6 col-md-offset-3" style="margin-top:3%;test-align:center"></div>');  
     $('#container').append('<div id="saberhandlediv" style="text-align: center;"><img id="imgsaberhandle" src="img/saberhandle.png" /></div>');
     $('#container').append('<div class="progress" style="border-collapse: collapse;box-shadow: 0px 0px 15px 5px rgba(162, 249, 0, .75);">');
     $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="'+ progress +'" aria-valuemin="0" aria-valuemax="100" style="width: '+ progress +'%;background-color: #A2F900;">');
